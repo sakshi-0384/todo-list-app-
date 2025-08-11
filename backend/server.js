5e6c5ed11510f76a8c2a9444d1e1ce272ffedf52
sakshi-0384 <zodesakshi2004@gmail.com>
2025-08-11 22:48:00 +0530

    Initial commit - todo app setup

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Server is ready")
+})
+
+app.listen(5000, () => {
+    connectDB();
+    console.log("Server started at http://localhost:5000");
+});
